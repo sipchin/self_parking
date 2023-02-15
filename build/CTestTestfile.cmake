2d0e6121379e913f0f33328c50aabb935b50bfaf
sipchin <sippakorn.po@ku.th>
2023-02-15 17:19:19 +0700

    Updated

--- build/CTestTestfile.cmake
+++ build/CTestTestfile.cmake
@@ -5,6 +5,12 @@
 # This file includes the relevant testing commands required for 
 # testing this directory and lists subdirectories to be tested as well.
 subdirs("gtest")
+subdirs("ReallyUsefulRobot/ROS/rur_navigation")
 subdirs("code")
 subdirs("robot_description_description")
+subdirs("ReallyUsefulRobot/ROS/rur_detailed_description")
+subdirs("rplidar_ros")
+subdirs("ReallyUsefulRobot/ROS/rur_gazebo")
+subdirs("ReallyUsefulRobot/ROS/rur_description")
+subdirs("ReallyUsefulRobot/ROS/rur")
 subdirs("mobile_robot_autonomous_navigation")
---
+++ build/ReallyUsefulRobot/ROS/rur_gazebo/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sipc/self_parking/src/ReallyUsefulRobot/ROS/rur_gazebo
+# Build directory: /home/sipc/self_parking/build/ReallyUsefulRobot/ROS/rur_gazebo
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
